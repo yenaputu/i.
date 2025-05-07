76aa8cd9ee1c2cf1a1ac452382dbf9dd42bf6adb
yenaputu <yenaputu@gmail.com>
2025-05-07 12:44:56 +0800

    Update python main.py

--- python main.py
+++
@@ -1,6 +0,0 @@
-/wedi-chatbot/
-├── main.py                # Backend Flask kamu
-├── templates/             # (Opsional, kalau pakai HTML dari Flask langsung)
-├── static/                # Untuk file statis seperti CSS, JS, dan gambar
-│   └── profil.jpg         # Gambar avatar kamu
-└── index.html             # Frontend HTML (jika terpisah dan dibuka manual)
